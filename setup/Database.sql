b310ce69dc6268dacfd4dfb6cc04d129d3b27961
Milan Kragujević <milankragujevic@gmail.com>
2018-02-16 18:40:46 +0100

    Update Database.sql

--- setup/Database.sql
+++ setup/Database.sql
@@ -16,7 +16,7 @@ CREATE TABLE `jobs` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
   `type` int(15) NOT NULL,
   `data` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
